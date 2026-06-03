25ae3419c607568e3a8ee54d7a2a20211cfa271e
Ramya-200420 <ketharamyasri9989@gmail.com>
2026-06-03 18:09:27 +0530

    update api url for production

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,9 +1,10 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api"
+  baseURL: "https://ecommerce-web-app-d6uc.onrender.com/api"
 });
 
+// Attach token automatically for every request
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem("token");
 
